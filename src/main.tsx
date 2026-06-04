f6e19086e9328ec0f16fa70c4f5c820213db6e2e
computas-hjm2 <hans-jacob.melby@computas.com>
2026-06-04 19:37:24 +0200

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ExpenseForm } from "./ExpenseForm";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ExpenseForm />
-  </React.StrictMode>
-);
